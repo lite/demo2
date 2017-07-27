017230fa9c6f6ab45e3acbe3b8f9ac617cae02ac
dli <litekok@gmail.com>
2017-07-27 15:09:44 +0800

    upload

---
+++ Assets/demo2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class demo2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Animator animator = GetComponent<Animator> ();
+		animator.speed = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
